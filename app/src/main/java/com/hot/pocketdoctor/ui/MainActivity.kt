d05c9420cb8000090601c0697a6ad0eb1490f9ae
Jaesung Lee <viki0114@naver.com>
2022-01-21 23:46:50 +0900

    layout : UI 레이아웃 구현 1 #2 * MainActivity, HomeFragment, HospitalDetailActivity 레이아웃 작업 완료 * CircleImageView 라이브러리 추가

--- app/src/main/java/com/hot/pocketdoctor/ui/MainActivity.kt
+++ app/src/main/java/com/hot/pocketdoctor/ui/MainActivity.kt
@@ -1,15 +1,51 @@
 package com.hot.pocketdoctor.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.hot.pocketdoctor.R
 import com.hot.pocketdoctor.databinding.ActivityMainBinding
+import com.hot.pocketdoctor.ui.home.HomeFragment
+import com.hot.pocketdoctor.ui.mypage.MyPageFragment
+import com.hot.pocketdoctor.ui.reports.ReportsFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment by lazy { HomeFragment() }
+    private val reportsFragment by lazy { ReportsFragment() }
+    private val myPageFragment by lazy { MyPageFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initBottomNavBar()
+    }
+
+    private fun initBottomNavBar() {
+        binding.bottomNavigationMain.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menu_home -> setCurrentFragment(homeFragment)
+                    R.id.menu_reports -> setCurrentFragment(reportsFragment)
+                    R.id.menu_myPage -> setCurrentFragment(myPageFragment)
+                }
+                true
+
+            }
+            selectedItemId = R.id.menu_home
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_frameLayout, fragment)
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            commit()
+        }
     }
 }
