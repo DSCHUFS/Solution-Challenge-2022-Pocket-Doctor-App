c56351798b422d561a37f646e5cbcea4032bc277
Jaesung Lee <viki0114@naver.com>
2022-01-27 21:41:19 +0900

    layout : 병원 리스트 레이아웃 구현 완료 #9 TabLayout + ViewPager2를 단순 View 스와이프로 일단 구현했으나 Fragment로 구현해야 할 것 같음 Fragment의 양이 너무 많아져서 이 부분은 고민 필요 closes #9

--- app/src/main/java/com/hot/pocketdoctor/ui/treatment/TreatmentListActivity.kt
+++ app/src/main/java/com/hot/pocketdoctor/ui/treatment/TreatmentListActivity.kt
@@ -2,14 +2,47 @@ package com.hot.pocketdoctor.ui.treatment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hot.pocketdoctor.R
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.hot.pocketdoctor.databinding.ActivityTreatmentListBinding
 
 class TreatmentListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTreatmentListBinding
+
+    private val viewPager: ViewPager2 by lazy { binding.treatmentListViewPager }
+    private val tabLayout: TabLayout by lazy { binding.treatmentListTabLayout }
+
+    private lateinit var treatmentList: List<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTreatmentListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setViewPager()
+        setTabLayout()
+    }
+
+    private fun setViewPager() {
+        treatmentList = listOf(
+            "Melancholia",
+            "Panic Disorder",
+            "Dementia",
+            "Eating Disorder",
+            "Insomnia",
+            "OCD",
+            "Anxiety",
+            "Etc"
+        )
+
+        val treatmentAdapter = TreatmentPagerAdapter(treatmentList)
+        viewPager.adapter = treatmentAdapter
+    }
+
+    private fun setTabLayout() {
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = treatmentList[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/hot/pocketdoctor/ui/treatment/TreatmentPagerHolder.kt
@@ -0,0 +1,12 @@
+package com.hot.pocketdoctor.ui.treatment
+
+import androidx.recyclerview.widget.RecyclerView
+import com.hot.pocketdoctor.databinding.ItemTreatmentListBinding
+
+class TreatmentPagerHolder(val binding: ItemTreatmentListBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bindViews(text: String) {
+        binding.textView.text = text
+    }
+
+}
