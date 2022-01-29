91454d03f6da2216f94644bd16ec50d64be55e3c
Jaesung Lee <viki0114@naver.com>
2022-01-30 02:59:49 +0900

    layout : 스플래시 화면 레이아웃 구현 #10

--- app/src/main/java/com/hot/pocketdoctor/ui/treatment/TreatmentPagerHolder.kt
+++ app/src/main/java/com/hot/pocketdoctor/ui/treatment/TreatmentPagerHolder.kt
@@ -6,7 +6,7 @@ import com.hot.pocketdoctor.databinding.ItemTreatmentListBinding
 class TreatmentPagerHolder(val binding: ItemTreatmentListBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bindViews(text: String) {
-        binding.textView.text = text
+//        binding.textView.text = text
     }
 
 }
