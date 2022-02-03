1020e6f610c75a6ef113b65f51f9dd182ccafa50
Jaesung Lee <viki0114@naver.com>
2022-02-03 21:31:52 +0900

    feat : Call FAB 클릭 시 전화연결 구현 * CALL_PHONE에 대한 권한 검사 추가 * 바로 전화되지 않도록 중간에 Dialog 추가

--- app/src/main/java/com/hot/pocketdoctor/ui/MainActivity.kt
+++ app/src/main/java/com/hot/pocketdoctor/ui/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.hot.pocketdoctor.ui
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import androidx.recyclerview.widget.RecyclerView
 import com.hot.pocketdoctor.R
 import com.hot.pocketdoctor.databinding.ActivityMainBinding
-import com.hot.pocketdoctor.databinding.ItemReportsRecyclerBinding
 import com.hot.pocketdoctor.ui.home.HomeFragment
 import com.hot.pocketdoctor.ui.mypage.MyPageFragment
-import com.hot.pocketdoctor.ui.reports.Reports
 import com.hot.pocketdoctor.ui.reports.ReportsFragment
 
 class MainActivity : AppCompatActivity() {
@@ -22,12 +23,89 @@ class MainActivity : AppCompatActivity() {
     private val reportsFragment by lazy { ReportsFragment() }
     private val myPageFragment by lazy { MyPageFragment() }
 
+    private val callFab by lazy { binding.mainCallFab }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBottomNavBar()
+        setCallFab()
+    }
+
+    private fun checkCallPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                showCallPopUp()
+            }
+
+            shouldShowRequestPermissionRationale(android.Manifest.permission.CALL_PHONE) -> {
+                showPermissionPopUp()
+            }
+
+            else -> {
+                requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE), CALL_REQUEST_CODE)
+            }
+        }
+    }
+
+    private fun showPermissionPopUp() {
+        AlertDialog.Builder(this)
+            .setTitle("Call Permission")
+            .setMessage("We need a Call Permission for dial to 1393")
+            .setPositiveButton("Accept") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE), CALL_REQUEST_CODE)
+            }
+            .setNegativeButton("Deny") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun showCallPopUp() {
+        AlertDialog.Builder(this)
+            .setTitle("Call 1393")
+            .setMessage("ASfdasdfadf")
+            .setPositiveButton("Accept") { _, _ ->
+                moveToDial()
+            }
+            .setNegativeButton("Deny") { _, _ -> }
+            .create()
+            .show()
+    }
+
+
+    private fun moveToDial() {
+        val phoneNumber = "1393"
+        val uri = Uri.parse("tel:${phoneNumber}")
+        val callIntent = Intent(Intent.ACTION_CALL, uri)
+        startActivity(callIntent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            CALL_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showCallPopUp()
+                }
+                else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show()
+                }
+            }
+            else -> {  }
+        }
+    }
+    private fun setCallFab() {
+        callFab.setOnClickListener {
+            checkCallPermission()
+        }
     }
 
     private fun initBottomNavBar() {
@@ -52,5 +130,9 @@ class MainActivity : AppCompatActivity() {
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
             commit()
         }
+    }
+
+    companion object {
+        const val CALL_REQUEST_CODE = 1000
     }
 }
