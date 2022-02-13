f01adda2d21f642854ff46658b739365f76a9ec0
Jaesung Lee <viki0114@naver.com>
2022-02-14 01:18:18 +0900

    [ADD] #30 - Navigation Component로 SignUpActivity 구조 변경

--- app/src/main/java/com/hot/pocketdoctor/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/com/hot/pocketdoctor/presentation/signup/SignUpActivity.kt
@@ -1,11 +1,13 @@
 package com.hot.pocketdoctor.presentation.signup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hot.pocketdoctor.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
